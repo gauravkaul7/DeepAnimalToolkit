0844ac4edb2e18984d106e3109aabbbaf23db01f
gauravkaul7 <gauravkaul7@gmail.com>
2021-11-16 16:50:41 -0500

    setup features

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,6 @@
 from .annotate_in_colab import sample_frames
 from .setup_colab_enviornment import setup_DeepAnimalToolkit
-from .visualize import visualizer
+#from .visualize import visualizer
 
 __all__ = ["sample_frames",
-           "setup_DeepAnimalToolkit",
-           "visualizer"]
+           "setup_DeepAnimalToolkit"]
