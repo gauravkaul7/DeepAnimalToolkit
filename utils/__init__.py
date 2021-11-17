a78b83aba2bfed01c4bf7fd5884201044bb5f31a
gauravkaul7 <gauravkaul7@gmail.com>
2021-11-17 15:35:21 -0500

    import error

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,7 @@
 from .setup_colab_enviornment import setup_DeepAnimalToolkit
 
 from .annotate_in_colab import sample_frames
-from .annotate_in_colab build_dataset
+from .annotate_in_colab import build_dataset
 
 from .colab_gui import annotate
 from .colab_gui import load_image_into_numpy_array
