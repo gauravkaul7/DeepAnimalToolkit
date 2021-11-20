be6bdd0838f9559644654e3ad1e65fb7038de393
gauravkaul7 <gauravkaul7@gmail.com>
2021-11-20 12:55:53 -0500

    updated model weight loading

--- detector/.ipynb_checkpoints/load_detector-checkpoint.py
+++ detector/.ipynb_checkpoints/load_detector-checkpoint.py
@@ -24,39 +24,37 @@ pretrained_weights = {
 
 class Detector:
     """
-    this class trains a faster rcnn with selected branches(keypoint,mask,etc.)
+    this class loads the weights of a faster rcnn with selected branches(keypoint,mask,etc.)
     branch: either "mask", "keypoint", or "none"
     """
 
-    def __init__(self, model_type: str):
+    def __init__(self, model_type: str, weights_path: str):
         self.cfg = get_cfg()
-        self.cfg.merge_from_file(
-            model_zoo.get_config_file(pretrained_weights[model_type])
-        )
+        self.cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  
+        self.cfg.merge_from_file(model_zoo.get_config_file(pretrained_weights[model_type]))
+        self.cfg.MODEL.WEIGHTS = "/content/drive/MyDrive/keypoint_dataset/pretrained_models/singleMouseTracker.pth"
         self.cfg.TEST.DETECTIONS_PER_IMAGE = 1
         self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
-        
-
-    def load_trained_model(self, weights_path: str):
-        self.cfg.MODEL.WEIGHTS = weights_path
-        self.detection_model = DefaultPredictor(self.cfg)
+        self.cfg.DATALOADER.NUM_WORKERS = 2
+        self.predictor = DefaultPredictor(self.cfg)
         print("loaded model from:", weights_path)
 
     def get_detections_video(self,video_path,num_frames):
-        num_frames = int(num_frames)
+        unfiltered_detections = []
+        frame_array = []
         cap = cv2.VideoCapture(video_path)
-        detections = []
-        
+        total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        #cap.set(2,1889)
+        print('total_frames:',total_frames)
+
         for f in tqdm(range(num_frames)):
             ret, frame = cap.read()
-            with torch.no_grad():
-                outputs = self.detection_model(frame)
+            outputs =  self.predictor(frame)
+            detections = outputs["instances"].to("cpu").pred_boxes.tensor.numpy() # np.array(detections)
+            out_scores = outputs["instances"].to("cpu").scores.numpy() # np.array(out_scores) 
+            print('detections:',detections)
+            unfiltered_detections.append([f,detections])
+        return unfiltered_detections
 
-            if len(outputs["instances"].to("cpu")) == 0:
-                detections.append([0, -1, -1, -1, -1])
-            else:
-                detections.append([1] + outputs["instances"].to("cpu").pred_boxes.tensor.tolist())
-        return detections
-    
     def get_detector(self):
         return self.detection_model
