d34cf1e52b0cbdf498ec04e16f512687b30857a7
gauravkaul7 <gauravkaul7@gmail.com>
2022-06-21 20:50:57 -0400

    vid_id foor tracing

--- utils/annotate_in_colab.py
+++ utils/annotate_in_colab.py
@@ -65,42 +65,32 @@ def setup_gui():
     
     return 
 
-def sample_frames(video_path,total_num_frames):
-    output_path = 'images/'
-    os.system('mkdir ' + output_path)
-    
-    if type(video_path) is not list:
-        video_paths = [video_path]
-    else:
-        video_paths = [ video_path + x for x in os.listdir(video_path) if x[-3:] == "avi"]
+def sample_frames(video_path,total_num_frames,output_path,vid_id=8888):
 
-    video_number = 0
-    for path in video_paths:
-        cap = cv2.VideoCapture(path)
-        num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-        for i in range(total_num_frames):
-            frame_no = random.randint(1, num_frames - 1)
-            cap = cv2.VideoCapture(path)
-            cap.set(1, frame_no)
-            ret, frame = cap.read()
-            cv2.imwrite(
-                output_path
-                + "video_"
-                + str(video_number)
-                + "_frame_"
-                + str(frame_no)
-                + ".jpg",
-                frame,
-            )
-        cap.release()
-        video_number += 1
+    if not os.path.isdir(output_path):
+        os.makedirs(output_path)
+
+    cap = cv2.VideoCapture(video_path)
+    num_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    for i in range(total_num_frames):
+        frame_no = random.randint(1, num_frames - 1)
+        cap = cv2.VideoCapture(video_path)
+        cap.set(1, frame_no)
+        __, frame = cap.read()
+        cv2.imwrite(
+            output_path
+            + "video_"
+            + str(vid_id)
+            + "_frame_"
+            + str(frame_no)
+            + ".jpg",
+            frame,
+        )
+    cap.release()
 
     print("sampling frames randomly")
     print("output image folder:",output_path)
+    
     return 
 
-def sample_frames_feature_spacing(video_paths):
-    print("sampling frames by finding a maximal spaning feature set")
-    print("output image folder:")
-    return 
 
